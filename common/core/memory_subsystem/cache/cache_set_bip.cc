6ac97294e0ecb3acbf52fa3bbc2fb2d1045f4803
Liam Strand <strandliam@gmail.com>
2025-01-28 21:20:54 -0600

    fix redefinition of LRU config

--- common/core/memory_subsystem/cache/cache_set_bip.cc
+++ common/core/memory_subsystem/cache/cache_set_bip.cc
@@ -1,5 +1,4 @@
 #include "cache_set_bip.h"
-#include "cache_set_lru.h"
 #include "log.h"
 #include "stats.h"
 
@@ -94,33 +93,4 @@ CacheSetBIP::moveToMRU(UInt32 accessed_index)
          m_lru_bits[i] ++;
    }
    m_lru_bits[accessed_index] = 0;
-}
-
-CacheSetInfoLRU::CacheSetInfoLRU(String name, String cfgname, core_id_t core_id, UInt32 associativity, UInt8 num_attempts)
-   : m_associativity(associativity)
-   , m_attempts(NULL)
-{
-   m_access = new UInt64[m_associativity];
-   for(UInt32 i = 0; i < m_associativity; ++i)
-   {
-      m_access[i] = 0;
-      registerStatsMetric(name, core_id, String("access-mru-")+itostr(i), &m_access[i]);
-   }
-
-   if (num_attempts > 1)
-   {
-      m_attempts = new UInt64[num_attempts];
-      for(UInt32 i = 0; i < num_attempts; ++i)
-      {
-         m_attempts[i] = 0;
-         registerStatsMetric(name, core_id, String("qbs-attempt-")+itostr(i), &m_attempts[i]);
-      }
-   }
-};
-
-CacheSetInfoLRU::~CacheSetInfoLRU()
-{
-   delete [] m_access;
-   if (m_attempts)
-      delete [] m_attempts;
 }
